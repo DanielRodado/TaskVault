2baaf14c0c13319468949e9175d68411d684de3c
DanielRodado <d4nielrodado@gmail.com>
2024-03-18 15:41:06 -0500

    App commit

---
+++ src/main/java/com/me/taskvault/TaskvaultApplication.java
@@ -0,0 +1,13 @@
+package com.me.taskvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskvaultApplication.class, args);
+	}
+
+}
