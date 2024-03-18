08a7eb0c29874b4e81ff19d0046797af43fad205
DanielRodado <d4nielrodado@gmail.com>
2024-03-18 16:06:42 -0500

    Conexion a base de datos de prueba (h2) + Creaión de datos de prueba

--- src/main/java/com/me/taskvault/TaskvaultApplication.java
+++ src/main/java/com/me/taskvault/TaskvaultApplication.java
@@ -1,13 +1,27 @@
 package com.me.taskvault;
 
+import com.me.taskvault.models.Person;
+import com.me.taskvault.repositories.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskvaultApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskvaultApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(PersonRepository personRepository) {
+		return args -> {
+
+			Person personOne = new Person("Daniel", "Rodado", 18);
+			personRepository.save(personOne);
+
+		};
 	}
 
 }
